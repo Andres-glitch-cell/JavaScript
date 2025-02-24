2c0920e13433f838d2434d4e1dbca2eaebaed889
«Andres» <afernandezs.iesjc@gmail.com>
2025-02-24 20:24:39 +0100

    BasicsOfJavaScript

---
+++ HelloJavaScript.js
@@ -0,0 +1,18 @@
+// Básico
+
+console.log("Hola Javascript")
+
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(6 / 2)
+console.log(5 * 2)
+console.log(10 ** 2)
+
+var variableJavaScript = "Hello Javascript with a variable"
+console.log(variableJavaScript)
+variableJavaScript = "Hello again Javascript with a variable"
+console.log(variableJavaScript)
+let holaJavaScriptOtraVez = "Hola JavaScript otra vez de distinto método"
+
+let enteroGrande = BigInt(2349849578 + 327487234832758)
+console.log(enteroGrande)
