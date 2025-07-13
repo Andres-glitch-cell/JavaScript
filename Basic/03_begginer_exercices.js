05286e7e86b43784a4332038b2a4f35950358e0e
Andrés Fernández <afernandezs.iesjc@gmail.com>
2025-07-13 15:43:51 +0200

    Variables And Operators Folders

---
+++ Basic/03_begginer_exercices.js
@@ -0,0 +1,16 @@
+// Este es un comentario en una linea
+
+/*
+Comentario en varias lineas
+*/
+
+// Declara variables de los tipos de datos primitivos en JavaScript
+let greeting = "Hola, JavaScript!";
+let number = 42;
+let isActive = true;
+let notAssigned;
+let emptyValue = null;
+let unsignedValue
+let uniqueId = Symbol("id");
+let bigNumber = 129873932087140318049109748142309314928n;
+
---
+++ Basic/04_operators.js
@@ -0,0 +1,20 @@
+let a = 4
+let b = 2
+
+console.log(a + b) // Suma
+console.log(a - b) // Resta
+console.log(a * b) // Multiplicación
+console.log(a / b) // División
+console.log(a % b) // Módulo (resto de la división)
+console.log(a ** b) // Exponente (a elevado a la b)
+
+a++
+console.log(a)
+
+b--
+console.log(b)
+
+let myVariable = 2
+console.log(myVariable)
+myVariable += 2
+console.log(myVariable)
