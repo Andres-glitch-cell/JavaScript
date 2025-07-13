484d81f4d2b09926253c44cdc2d5ccb7cdd6de83
Andrés Fernández <afernandezs.iesjc@gmail.com>
2025-07-13 13:43:17 +0200

    BasicFolderDone

--- Basic/00_helloWorld.js
+++ Basic/00_helloWorld.js
@@ -7,12 +7,11 @@ console.log(5 - 2)
 console.log(6 / 2)
 console.log(5 * 2)
 console.log(10 ** 2)
-
-var variableJavaScript = "Hello Javascript with a variable"
-console.log(variableJavaScript)
-variableJavaScript = "Hello again Javascript with a variable"
-console.log(variableJavaScript)
-let holaJavaScriptOtraVez = "Hola JavaScript otra vez de distinto método"
-
-let enteroGrande = BigInt(2349849578 + 327487234832758)
-console.log(enteroGrande)
+console.log(5 % 2)
+console.log(5 + 2 * 3)
+console.log(5 + 2 * 3 - 4 / 2)
+console.log((5 + 2) * (3 - 4) / 2)
+console.log("Hola " + "Javascript")
+console.log("Hola " + 5 + 2)
+console.log("Hola " + (5 + 2))
+console.log("Hola " + 5 ** 2) // Esto es 5 al cuadrado
